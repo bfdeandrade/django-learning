4166d436c76d5120f49cab1db9d5a9fde12e0bc0
Breno Andrade <bfdeandrade@gmail.com>
2024-11-17 12:23:13 -0300

    add new app

--- dj_learning/dj_learning/urls.py
+++ dj_learning/dj_learning/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('<h1>Hello, <p>World!</p></h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', my_view),
 ]
